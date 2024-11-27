a9050e8562f8e7d5be05ebb42680a68f8df917e5
alfhkr761@gmail.com <126091177+Arlkihfa@users.noreply.github.com>
2024-11-27 23:14:38 +0700

    mengganti nama file

--- app/src/main/java/com/example/heartguardlayout/detailRME.kt
+++ app/src/main/java/com/example/heartguardlayout/detailRME.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class historyRME : AppCompatActivity() {
+class detailRME : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_history_rme)
+        setContentView(R.layout.activity_detail_rme)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
